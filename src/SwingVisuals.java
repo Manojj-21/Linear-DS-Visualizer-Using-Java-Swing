0b3095574c3fe6305166eb82703f11d86672811b
Manojj-21 <manojj2136@gmail.com>
2025-04-22 18:33:12 +0530

    Initial commit

---
+++ src/SwingVisuals.java
@@ -0,0 +1,48 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class SwingVisuals {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SwingVisuals window = new SwingVisuals();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SwingVisuals() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(66, 29, 96, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+	}
+}
